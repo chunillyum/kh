ab953709b5651884eb88d725069436f0e985c110
chunillyum <otercc2@naver.com>
2023-05-30 12:46:00 +0900

    단축키 개꿀팁
    ㅇㅇㅇ

---
+++ jjangva/src/day1/sample/javaSample.java
@@ -0,0 +1,12 @@
+package day1.sample; //c드라이브 gitgg폴더 짱바
+
+public class javaSample {
+
+	public static void main(String[] args) {
+		System.out.println("Hello java!");
+		//쉬프트 방향키 = 텍스트 잡기
+		//컨트롤 방향키 = 문장 이동
+		//쉬프트+컨트롤 방향키 = 텍스트 잡은 채로 문장 이동
+	}
+
+}
