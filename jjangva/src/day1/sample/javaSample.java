8d0fbe1065a669f04f77f87bede0b48cbcef701a
chunillyum <otercc2@naver.com>
2023-05-31 08:56:33 +0900

    Update javaSample.java

--- jjangva/src/day1/sample/javaSample.java
+++ jjangva/src/day1/sample/javaSample.java
@@ -4,6 +4,7 @@ public class javaSample {
 
 	public static void main(String[] args) {
 		System.out.println("Hello java!");
+		//test!!!
 		//쉬프트 방향키 = 텍스트 잡기
 		//컨트롤 방향키 = 문장 이동
 		//쉬프트+컨트롤 방향키 = 텍스트 잡은 채로 문장 이동
