764634e76b4fa6952d16480fac8aebae0ab882e8
chunillyum <otercc2@naver.com>
2023-05-31 12:39:37 +0900

    ㅣㅏㅣㅏ

---
+++ jjangva/src/castEx.java
@@ -0,0 +1,16 @@
+
+public class castEx {
+
+	public static void main(String[] args) {
+		//3.14는 더블형 리터럴이기 때문에 int에 저장할 수 없음. 강제 형변환을 해야함
+		int pi = (int)3.14;
+		//3은 int형 리터럴이기 때문에 더블형 정수에 저장할 수 있음. 자동 형변환
+		double num1 = 3;
+		int num2 = (int)123L;
+		
+		System.out.println((double)1/2);
+		//1/2는 문법에선 문제가 없지만 원하는 결과인 0.5가 안 나오기 때문에 강제형변환을 하였음
+		
+	}
+
+}
---
+++ jjangva/src/strStr.java
@@ -0,0 +1,17 @@
+
+public class strStr {
+
+	public static void main(String[] args) {
+		final int MAX_AGE;
+		
+		//MAX_AGE : 상수, 100; 리터럴
+		MAX_AGE = 100;
+		//MAX AGE = 101;//에러 발생. 1번 조기화 한 후 수정하려고 했기 때문에
+		System.out.println(MAX_AGE);
+		//지역 변수는 자동으로 조기화가 안됨
+		int totalCount;
+		//totalCount 변수가 조기화 되어있지 않기 때문에 사용이 물가능 => 메리 발생
+		//System.out.printIn(totalCount);
+	}
+	
+}
