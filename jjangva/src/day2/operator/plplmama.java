1fb5ed7dae4ac343fe4678eee08c3f2bd167b1c6
chunillyum <otercc2@naver.com>
2023-05-31 15:12:28 +0900

    증감연산자 예제

---
+++ jjangva/src/day2/operator/plplmama.java
@@ -0,0 +1,20 @@
+package day2.operator;
+
+public class plplmama {
+
+	public static void main(String[] args) {
+		int num1 = 10, num2 = 10;
+		System.out.println("작업 전 : "+num1+"(전위형),"+num2+	"(후위형)");
+		System.out.println("작업 중 : "+ ++num1+"(전위형),"+num2++ +"(후위형)");
+		System.out.println("작업 후 : "+num1+"(전위형),"+num2+	"(후위형)");
+		
+		
+		System.out.println("===================");
+		
+		System.out.println();
+		
+		
+		
+	}
+
+}
