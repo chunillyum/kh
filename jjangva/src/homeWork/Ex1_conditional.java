7850eca84488665c112c3b036d6d12cf2c3cccfd
chunillyum <otercc2@naver.com>
2023-06-01 15:11:35 +0900

    숙제 1 조건 연산문

---
+++ jjangva/src/homeWork/Ex1_conditional.java
@@ -0,0 +1,28 @@
+package homeWork;
+import java.util.Scanner;
+
+public class Ex1_conditional {
+//숙제1
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("성별을 입력하세요.");
+		String sex = sc.nextLine();
+		System.out.println("여성입니까?" + (sex.equalsIgnoreCase("W") ? true:false));
+		
+		
+		
+		
+		
+//		성별(M:남성,W:여성)를 입력받아 여성인지 확인하는 코드를 작성하세요. 조건 연산자 이용
+//		(package : day3.homework, file : Ex1_conditional) 
+//		input gender : 
+//		M
+//		Are you a women? false
+//
+//		input gender : 
+//		W
+//		Are you a women? true
+
+	}
+
+}
