c346c11ee54c2558ac3bdfb4951596b4e2b18322
chunillyum <otercc2@naver.com>
2023-06-02 14:34:39 +0900

    삼항연산 홀짝

---
+++ jjangva/src/day4_work/EvenNumberEx2.java
@@ -0,0 +1,12 @@
+package day4_work;
+
+public class EvenNumberEx2 {
+
+	public static void main(String[] args) {
+		int num = 4;
+		String result;
+		System.out.println(num%2==0 ? result = "even" : "odd");
+//		System.out.println(num + " is a " + result + " number."); 삼항연산 사용하면 이뻐진다는 뜻
+	}
+
+}
