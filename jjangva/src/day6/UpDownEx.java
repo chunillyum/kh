5f7eeeebe3f6013c86511db50554338dc7fe0438
chunillyum <otercc2@naver.com>
2023-06-08 08:48:21 +0900

    컷ㅋ

--- jjangva/src/day6/UpDownEx.java
+++ jjangva/src/day6/UpDownEx.java
@@ -3,15 +3,14 @@ package day6;
 import java.util.Scanner;
 
 public class UpDownEx {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int random = (int)(Math.random()*100)+1;
-		System.out.println("숫자를 맞춰보세요!");
+		System.out.println(random + "숫자를 맞춰보세요!");
 		int answer = 0;
-		for(;answer != random;) {
+		for(;answer != random;) {//for문에서 세미콜론은 반드시 2개를 써야함. 그래서 첫번째 ;을 쓴 것.
 			answer = sc.nextInt();
-			sc.nextLine();
+			sc.nextLine();//문제 해결이 안 된 원인. 개행문자 소비를 안 해서 25줄의 입력값이 입력되지 않음.
 		if(answer<random) {
 			System.out.println("up!");
 		}
@@ -20,13 +19,16 @@ public class UpDownEx {
 		}
 		else if(answer==random) {
 			System.out.println("Good!");
-			System.out.println("want more?\ny/n");
+			System.out.println("continue?\ny/n");
+		if(answer==random) {
+			String con=sc.nextLine();
+		if (con.equals("y")) {
+			answer=0;
+			System.out.println("숫자를 맞춰보세요!");
 		}
-		String y = sc.nextLine();
-		while(y != "n") {
-			continue;
-		
 		}
-}
+		}
+		}
 	}
 }
+//!sc.nextLine().equals("y")
