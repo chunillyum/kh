f9413cb90d605c3a167148a9580d3e3cf66f7285
chunillyum <otercc2@naver.com>
2023-06-11 04:59:03 +0900

    season과제 중복메소드 제거

--- jjangva/src/day8/MethodSeasonEx.java
+++ jjangva/src/day8/MethodSeasonEx.java
@@ -20,28 +20,11 @@ public class MethodSeasonEx {
 			return "잘못된 입력입니다.";
 		}
 	}
-	public static void getSeason(int month) {
-		if(month==12 || month<3) {
-			System.out.println("겨울입니다.");
-		}
-		else if(month>=3 && month<6) {
-			System.out.println("봄입니다.");
-		}
-		else if(month>=6 && month<9) {
-			System.out.println("여름입니다.");
-		}
-		else if(month>=9 && month<12) {
-			System.out.println("가을입니다.");
-		}
-		else {
-			System.out.println("잘못된 입력입니다.");
-		}
-	}
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("계절을 입력해주세요");
 		int month = sc.nextInt();
-		getSeason(month);
 		System.out.println(season(month));
 	}
 
