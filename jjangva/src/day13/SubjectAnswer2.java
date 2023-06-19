586094ec2a987dbb613e172018b8a909d1dc26c7
chunillyum <otercc2@naver.com>
2023-06-19 15:04:39 +0900

    ㅜㅜ

--- jjangva/src/day13/SubjectAnswer2.java
+++ jjangva/src/day13/SubjectAnswer2.java
@@ -4,8 +4,8 @@ public class SubjectAnswer2 {
 
 	public static void main(String[] args) {
 
-		SubjectAnswer sub1 = new SubjectAnswer("kor", 1, 2);
-		sub1.updateScore(30, 20, 50);//순서를 print 아래에 놓으면 에러남.
+		SubjectAnswer sub1 = new SubjectAnswer("kor", 1, 3);
+		sub1.updateScore(30, 20, 60);//순서를 print 아래에 놓으면 에러남.
 		sub1.print();
 		
 		System.out.println("===============");
